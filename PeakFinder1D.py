ed3eaab291fdf5d55707d547de7599161219bc5a
dariacher <32611360+dariacher@users.noreply.github.com>
2018-10-15 19:56:40 +0300

    Update PeakFinder1D.py

--- PeakFinder1D.py
+++ PeakFinder1D.py
@@ -1,1 +1,13 @@
-
+def peakFinder(array):
+    n = len(array)//2
+    if len(array) == 2:
+        if array[0] > array[1]:
+            return array[0]
+        else: 
+            return array[1]
+    if array[n-1] > array[n]:
+        return peakFinder(array[:n])
+    elif array[n+1] > array[n]:
+        return peakFinder(array[n+1:])
+    else: 
+        return array[n]
